06675d7a32820300bf6f920c46c64be0d0a74cde
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-27 11:51:55 -0300

    Funcionalidad para actualizar comentarios

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -43,11 +43,65 @@ export const createComment = async (req , res) =>{
 }
 
 export const updateComment = async (req , res) =>{
-    
+    // Validar existencia del parámetro en la URL
+    try{
+        const id = req.params.id;
+        if (!id || parseInt(isNaN(id))) {
+            return res.status(400).json({ status: 'error', message: 'comment ID is required and must be a number' });
+        }
+        // Validar el body
+        if (!req.body || typeof req.body.content !== 'string') {
+          return res.status(400).json({ status: 'error', message: 'content is required' });
+        }
+        
+        const content = req.body.content;
+        const user_id = req.user.id;
+
+        
+        //Validar la existencia del comentario a actualizar
+        const commentExits = await Comment.findOne({ where: { id:id, user_id:user_id } });
+
+        if(!commentExits){
+          return res.status(404).json({ status: 'error', message: 'comment not found.'});
+        }
+        
+        //Actualizar el comentario
+        commentExits.content = content;
+        await commentExits.save();
+
+        return res.status(200).json({status: 'success',message: 'comment update successfully' });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({status:'error', message:'internal server error',error});
+    }
 }
 
 export const deleteComment = async (req , res) =>{
-    
+    // Validar existencia del parámetro en la URL
+    try{
+        const id = req.params.id;
+        if (!id || parseInt(isNaN(id))) {
+            return res.status(400).json({ status: 'error', message: 'comment ID is required and must be a number' });
+        }
+        
+        const user_id = req.user.id;
+
+        
+        //Validar la existencia del comentario a borrar
+        const commentExits = await Comment.findOne({ where: { id:id, user_id:user_id } });
+
+        if(!commentExits){
+          return res.status(404).json({ status: 'error', message: 'comment not found.'});
+        }
+        
+        //Borra el comentario
+        await commentExits.destroy();
+
+        return res.status(200).json({status: 'success',message: 'comment delete successfully' });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({status:'error', message:'internal server error',error});
+    }
 }
 
 export const getCommentsByPost = async (req , res) =>{
