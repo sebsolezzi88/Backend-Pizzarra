48c8f3fe8bbc2fc4b864f4aabdbd4e8166dc539f
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-27 10:42:31 -0300

    Creando base para hacer la funciones

---
+++ controllers/commentControllers.js
@@ -0,0 +1,15 @@
+export const createComment = async (req , res) =>{
+
+}
+
+export const updateComment = async (req , res) =>{
+    
+}
+
+export const deleteComment = async (req , res) =>{
+    
+}
+
+export const getCommentsByPost = async (req , res) =>{
+    
+}
