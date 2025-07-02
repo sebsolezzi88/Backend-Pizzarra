e95dd11765c150bd6fd5797404fc1e4da3a7ad47
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-02 10:45:01 -0300

    Correción para traer el nombre de usuario del creador del post

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -148,7 +148,13 @@ export const getPostsByUserId = async (req, res) => {
 
 export const  getAllPost = async (req ,res) =>{
     try {
-        const posts = await Post.findAll({ order: [['createdAt', 'DESC']]});
+        const posts = await Post.findAll({ 
+          order: [['createdAt', 'DESC']],
+          include: {
+            model: User,
+            attributes: ['username'] 
+      }
+        });
         return res.status(200).json({status:'success', message:'posts obtained',posts});
     } catch (error) {
         return res.status(500).json({status:'error', message:'internal server error',error});
